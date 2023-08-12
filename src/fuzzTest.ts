4a23032ade5b6396157bfbdd35372108088b49f9
Duknam Yoo <yoo2001818@gmail.com>
2023-08-12 15:33:52 +0900

    Add fuzz testing utility

---
+++ src/fuzzTest.ts
@@ -0,0 +1,87 @@
+import { CPU } from './cpu/cpu';
+import crypto from 'crypto';
+import { Memory } from './memory/types';
+
+function formatResultData(input: Uint8Array): string[] {
+  const output: string[] = [];
+  output.push(
+    `PC: ${(input[8] | (input[9] << 8)).toString(16).padStart(4, '0')} ` +
+    `SP: ${(input[10] | (input[11] << 8)).toString(16).padStart(4, '0')} ` +
+    `A: ${(input[0]).toString(16).padStart(2, '0')} ` +
+    `B: ${(input[1]).toString(16).padStart(2, '0')} ` +
+    `C: ${(input[2]).toString(16).padStart(2, '0')} ` +
+    `D: ${(input[3]).toString(16).padStart(2, '0')} ` +
+    `E: ${(input[4]).toString(16).padStart(2, '0')} ` +
+    `F: ${(input[5]).toString(16).padStart(2, '0')} ` +
+    `H: ${(input[6]).toString(16).padStart(2, '0')} ` +
+    `L: ${(input[7]).toString(16).padStart(2, '0')}`
+  );
+  for (let i = 12; i < input.length - 3; i += 4) {
+    const addr = input[i] | (input[i + 1] << 8);
+    const rw = input[i + 2];
+    const value = input[i + 3];
+    if (rw === 0) break;
+    output.push(`${rw === 1 ? 'R' : 'W'} $${addr.toString(16).padStart(4, '0')}: ${value.toString(16).padStart(2, '0')}`);
+  }
+  return output;
+}
+
+function testImpl(input: Uint8Array): Uint8Array {
+  const output = new Uint8Array(40);
+  let readPos = 12;
+  let writePos = 12;
+  const memory: Memory = {
+    read: (addr) => {
+      const readValue = input[readPos];
+      output[writePos] = addr & 0xff;
+      output[writePos + 1] = (addr >> 8) & 0xff;
+      output[writePos + 2] = 1;
+      output[writePos + 3] = readValue;
+      writePos += 4;
+      readPos += 1;
+      return readValue;
+    },
+    write: (addr, value) => {
+      output[writePos] = addr & 0xff;
+      output[writePos + 1] = (addr >> 8) & 0xff;
+      output[writePos + 2] = 2;
+      output[writePos + 3] = value;
+      writePos += 4;
+    },
+  };
+  const cpu = new CPU(memory);
+  cpu.isDebugging = true;
+  // Set up testing environment
+  for (let i = 0; i < 8; i += 1) {
+    cpu.registers[i] = input[i];
+  }
+  cpu.registers[8] = input[8] | (input[9] << 8);
+  cpu.registers[9] = input[10] | (input[11] << 8);
+  // Run test
+  cpu.step();
+  // Validate results
+  for (let i = 0; i < 8; i += 1) {
+    output[i] = cpu.registers[i];
+  }
+  output[8] = cpu.registers[8] & 0xff;
+  output[9] = (cpu.registers[8] >>> 8) & 0xff;
+  output[10] = cpu.registers[9] & 0xff;
+  output[11] = (cpu.registers[9] >>> 8) & 0xff;
+  const debugLine = cpu.debugLogs[0];
+  if (debugLine != null) {
+    console.log(debugLine.data, debugLine.comment);
+  }
+  return output;
+}
+
+function runTest(): void {
+  while (true) {
+    const input = crypto.randomBytes(16);
+    console.log(input.toString('hex'));
+    const output = testImpl(input);
+    console.log(formatResultData(output).join('\n'));
+    console.log(Buffer.from(output).toString('hex'));
+  }
+}
+
+runTest();
